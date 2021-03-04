01df633fa8ca662595af057ad79d0cec12588f25
Joshua Bidwell <jjbidwell262@gmail.com>
2021-03-03 16:29:56 -0800

    added routes with static file integration

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3001;
+
+const root = path.join(__dirname, "public")
+app.use(express.static(root));
+
+//console.log(path.join(__dirname , "public"))
+
+
+app.get("/", (req, res) => {
+    res.sendFile("about.html", {root : root});
+})
+
+app.get("/skills", (req, res) => {
+    res.sendFile("skills.html", {root : root});
+})
+
+app.get("/portfolio", (req, res) => {
+    res.sendFile("portfolio.html", {root : root});
+})
+
+app.get("/contact", (req, res) => {
+    res.sendFile("contact.html", {root : root});
+})
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`))
