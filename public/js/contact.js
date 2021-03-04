3d6a2960ed302ae78629473f3c35749b6d4ec4f1
Joshua Bidwell <jjbidwell262@gmail.com>
2021-03-04 10:42:01 -0800

    The page now sends a formatted email, now I just need to add verification and redirect the user

--- public/js/contact.js
+++ public/js/contact.js
@@ -5,4 +5,12 @@ document.getElementById("submit").addEventListener("click", (event)=> {
         email: $("#email").val(),
         message: $("#message").val()
     }
+    $.ajax({
+        url: "/contact",
+        method: "POST",
+        data: contact
+    }).then(()=>{
+        alert("Thank you for contacting me, I will be sure to get back to you as soon as I can")
+        location.replace("/")
+    })
 })
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 const nodeMailer = require("nodemailer");
 
@@ -7,25 +8,23 @@ const nodeMailer = require("nodemailer");
 const app = express();
 const port = process.env.PORT || 3001;
 
-// const transporter = nodeMailer.createTransport({
-//     host: 'smtp.mail.yahoo.com',
-//     port: 465,
-//     service: 'yahoo',
-//     secure: false,
-//     auth: {
-//         user: process.env.YAHOO_EMAIL,
-//         pass: process.env.YAHOO_PASSWORD
-//     },
-//     debug: false,
-//     logger: true
-// })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// const mailOptions = {
-//     from: process.env.YAHOO_EMAIL,
-//     to: process.env.GMAIL_EMAIL,
-//     subject: "TEST",
-//     text: "Bottom test"
-// }
+
+const transporter = nodeMailer.createTransport({
+    host: 'smtp.mail.yahoo.com',
+    port: 465,
+    service: 'yahoo',
+    secure: false,
+    auth: {
+        user: process.env.YAHOO_EMAIL,
+        pass: process.env.YAHOO_PASSWORD
+    },
+    debug: false,
+    logger: true
+})
+
 
 // transporter.sendMail(mailOptions, (err, info) => {
 //     if (err) {
@@ -53,6 +52,26 @@ app.get("/portfolio", (req, res) => {
 
 app.get("/contact", (req, res) => {
     res.sendFile("contact.html", { root: root });
+})
+
+app.post("/contact", (req, res) => {
+    const mailOptions = {
+        from: process.env.YAHOO_EMAIL,
+        to: process.env.GMAIL_EMAIL,
+        subject: "Message from Portfolio",
+        text: "This is a message from your portfolio contact page",
+        html: `<p><b>Name</b>: ${req.body.name}</p>
+               <p><b>Email</b>: ${req.body.email}</p>
+               <p><b>Message</b>: ${req.body.message}</p>`
+    }
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("Message sent!")
+        }
+    })
+
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}.`))
