b13a8cf0953b2c9eea3551af1d886c067a69012a
Joshua Bidwell <jjbidwell262@gmail.com>
2021-03-04 10:08:18 -0800

    Clicking submit now creates object with user's name, email, and message

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,1 +1,8 @@
-console.log("hello");
+document.getElementById("submit").addEventListener("click", (event)=> {
+    event.preventDefault();
+    const contact = {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        message: $("#message").val()
+    }
+})
